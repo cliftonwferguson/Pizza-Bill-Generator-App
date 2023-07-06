03c81beffd32c92131660b7d2f977e4657c2391e
Clifton Ferguson <clifton.ferguson7@gmail.com>
2023-07-06 14:18:40 -0500

    pushing main

---
+++ src/pizza/Main.java
@@ -0,0 +1,8 @@
+package pizza;
+
+public class Main {
+	public static void main(String[] args) {
+          Pizza basePizza = new Pizza(false);
+          basePizza.getPizzaPrice();
+	}
+}
---
+++ src/pizza/Pizza.java
@@ -0,0 +1,20 @@
+package pizza;
+
+public class Pizza {
+	private int price;
+	private Boolean veg;
+
+	public Pizza(Boolean veg) {
+		super();
+		this.veg = veg;
+		if (this.veg) {
+			this.price = 300;
+		} else {
+			this.price = 400;
+		}
+	};
+
+	public void getPizzaPrice() {
+		System.out.println(this.price);
+	};
+}
