f3ad2523e610f07273a64787c2af357c718fa1d2
Clifton Ferguson <clifton.ferguson7@gmail.com>
2023-07-07 11:53:26 -0500

    adding values to create the bill calc

--- src/pizza/Pizza.java
+++ src/pizza/Pizza.java
@@ -7,9 +7,9 @@ public class Pizza {
 	private int extraToppingsAdded = 150;
 	private int backPackPrice = 20;
 	private int basePizzaPrice;
-	private boolean isExtraCheeseAdded;
-	private boolean isExtraToppingsAdded;
-	private boolean isOptedForTakeaway;
+	private boolean isExtraCheeseAdded = false;
+	private boolean isExtraToppingsAdded = false;
+	private boolean isOptedForTakeaway = false;
 	
 
 	public Pizza(Boolean veg) {
@@ -24,16 +24,19 @@ public class Pizza {
 	};
 
 	public void addExtraCheese() {
+		isExtraCheeseAdded = true;
 		System.out.println("Extra cheese added");
 		this.price += extraCheesePrice;
 	}
 	
 	public void addExtraToppings () {
+		isExtraToppingsAdded = true;
 		System.out.println("Extra toppings added");
 		this.price += extraToppingsAdded;
 	}
 	
 	public void takeAway() {
+		isOptedForTakeaway=true;
 		System.out.println("Take away opted");
 		this.price += backPackPrice;
 	}
